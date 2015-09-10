d820cdb9afe2e7729199afff99eec8e48c88ba93
shwetapurushe <shwetapurushe@gmail.com>
2015-09-10 18:49:51 -0400

    getting stuff from master for demo

---
+++ src/collapsible_directive.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Shweta on 9/10/2015.
+ */
+(function(){
+    angular.module('spCollapse').directive('collapsePanel',collapsePanelComponent );
+
+    function collapsePanelComponent (){
+        return {
+            restrict : 'E',
+            scope : {
+                urlstring : '@',
+                heading : '@'
+            },
+            templateUrl : 'src/collapsible_Panel_Partial.html',
+            controller : collapseController,
+            controllerAs : 'coCtrl',
+            bindToController: true,
+            link : function (scope, elem, attrs){
+            }
+        }// end of ddo
+    }
+
+    //collapseController.$inject= ['$scope'];
+    function collapseController (){
+        var coCtrl = this;
+
+        coCtrl.isCollapsed = true;
+        coCtrl.collapseContent = collapseContent;
+
+        function collapseContent (){
+            coCtrl.isCollapsed = ! coCtrl.isCollapsed;
+        }
+    }
+})();
